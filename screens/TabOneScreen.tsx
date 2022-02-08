5848c169b69be28db778376ff40be10b1a6344a9
adammathewvaughn <adammathewvaughn@gmail.com>
2022-02-07 23:32:11 -0600

    apparently null is not an object

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,27 +1,28 @@
 import * as React from 'react';
-im
-import styled from "styled-components/native";
+import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
+
 import { Text, View } from '../components/Themed';
 import Video from "react-native-video";
+
 
    
 
 export default function TabOneScreen() {
   return (
-    <View style={styled.container}>
+    <View style={styles.container}>
         <Video
           source={require("../assets/20201118_162915.mp4")}
-          style={styled.backgroundVideo}
+          style={styles.container}
           muted={true}
           repeat={true}
           resizeMode={"cover"}
           rate={1.0}
           ignoreSilentSwitch={"obey"}   
         />  
-      <Text style={styled.title}>Oh, shit! I made an Android app!</Text>
-      <View style={styled.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Look, Ma! I made an Android app!</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
             </View>
   );
