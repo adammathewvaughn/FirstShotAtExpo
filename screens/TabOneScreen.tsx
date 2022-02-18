5768855843115586274104f9cecaac3dc9854478
adammathewvaughn <adammathewvaughn@gmail.com>
2022-02-17 23:44:09 -0600

    added images"

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,28 @@
 import * as React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground  } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 
-import { Text, View } from '../components/Themed';
-import Video from "react-native-video";
+import { Text, View,} from '../components/Themed';
+import { Video } from 'expo-av';
 
 
    
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require("../assets/images/logo.png")}style= {styles.container}>
-        <Video
-          source={require("../assets/20201118_162915.mp4")}
-          style={styles.container}
-          muted={true}
-          repeat={true}
-          resizeMode={"cover"}
-          rate={1.0}
-          ignoreSilentSwitch={"obey"}   
-        />  
-      <Text style={styles.title}>Look, Ma! I made an Android app!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-            </ImageBackground>
-            </View>
+      <View style={styles.container}>
+        <ImageBackground source={require("../assets/images/icon.png")}style= {styles.container}>
+          <Video
+              source={require("../assets/20201118_162915.mp4")}
+              style={styles.container}
+              resizeMode={"cover"}
+               /> 
+        <Text style={styles.title}>Look, Ma! I made an Android app!</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+              </ImageBackground>
+              </View>
   );
 }
 
